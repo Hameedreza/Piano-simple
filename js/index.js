bfd9f1a8b4c064fbbbe11baf034d95ae728fa5cd
Hameedreza Gucci <hamidguc96@gmail.com>
2022-02-13 22:37:17 +0330

    This is some piano exercise.

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const keyboard = document.querySelector('.piano__keyboard');
+let pianoNotes = ['C' , 'D' , 'E' ,'F' , 'G' , 'A' , 'B'];
+
+
+const createKey = (type , note , octave)=>{
+  const key = document.createElement('button');
+  key.className = `piano__key piano__key--${type}`; 
+  key.dataset.letterNote = type == 'white' ? note + octave : note + '#' + octave; //setAttribute
+  key.dataset.letterNoteFileName = type == 'white' ? note + octave : note + 's' + octave;
+  key.textContent =key.dataset.letterNote;
+
+  key.addEventListener('mousedown' , ()=>{
+    playSound(key);
+    key.classList.add('piano__key--playing');
+  })
+  key.addEventListener('mouseup', ()=>{
+    key.classList.remove('piano__key--playing')
+  })
+  key.addEventListener('mouseleave' , ()=>{
+    key.classList.remove('piano__key--playing')
+  })
+
+  return key;
+}
+
+let init = ()=>{
+  for(let i=1 ;i<=5 ; i++ ){
+    for(let j=0 ; j<7 ; j++){
+      let key = createKey('white' , pianoNotes[j] , i);
+      keyboard.appendChild(key);
+
+      if(j!=2 && j!=6 ){
+        key = createKey('black' , pianoNotes[j] , i);
+        let emptySpace = document.createElement('div');
+        emptySpace.className = 'empty-space';
+        emptySpace.appendChild(key);
+        keyboard.appendChild(emptySpace);
+      }
+    }
+  }
+
+}
+
+const playSound = (key)=>{
+  const audio = document.createElement('audio');
+  audio.src = 'sounds/' + key.dataset.letterNoteFileName + '.mp3';
+  audio.play().then(()=> audio.remove());
+}
+
+
+
+init();
